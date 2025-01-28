516f5adfcbde01c7c5668a2c919239dabf00dbd8
Lukas Keller <keller.lukas@bluewin.ch>
2025-01-28 21:37:32 +0100

    delete memory file

--- src/py/deployment/scripts/remember_wohnzimmer/remember_wohnzimmer.py
+++ src/py/deployment/scripts/remember_wohnzimmer/remember_wohnzimmer.py
@@ -1,3 +1,4 @@
+import os
 from src.py.deployment.utils.remember_states import RememberStates
 from src.py.deployment.scripts.remember_wohnzimmer.config import light_ids, memory_file
 
@@ -7,6 +8,7 @@ def main():
     rem_states = RememberStates(light_ids=light_ids)
     rem_states.record()
     rem_states.to_file(memory_file)
+    os.remove(memory_file)
     
 if __name__ == "__main__":
     main()
